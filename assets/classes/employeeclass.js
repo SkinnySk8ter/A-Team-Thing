caa6c2a20bd9384266bf8ab4e27a3b4043edab0a
Jody Jones <jodybjones@yahoo.com>
2020-01-03 23:13:18 -0500

    started creating classes/subclasses

---
+++ assets/classes/employeeclass.js
@@ -0,0 +1,21 @@
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    get_name() {
+        return this.name;
+    }
+
+    get_email() {
+        return this.email;
+    }
+
+    get_role() {
+        return "Employee";
+    }
+
+}
+
+module.exports = Employee;
